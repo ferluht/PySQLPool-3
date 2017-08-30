29b53952593ae17a1019efb0fd904d3270d99066
Ilya Belikov <ilya.belikov@phystech.edu>
2017-08-30 12:00:31 +0300

    forked from https://pypi.python.org/pypi/PySQLPool

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+__version__ = '0.3.8'
+__author__ = 'Nick Verbeck'
+__author_email__ = 'nerdynick@gmail.com'
+
+setup(name='PySQLPool',
+      version=__version__,
+      author=__author__,
+      author_email=__author_email__,
+      license='LGPL V3',
+      platforms=['ALL'],
+      description='Python MySQL Connection Pooling and MySQL Query management',
+      url='https://github.com/nerdynick/PySQLPool/',
+      download_url="http://code.google.com/p/pysqlpool/downloads/list",
+      classifiers = [
+                     'Topic :: Software Development :: Libraries :: Python Modules',
+                     'Topic :: Database',
+                     'Programming Language :: Python',
+                     'Operating System :: OS Independent',
+                     'Development Status :: 5 - Production/Stable'],
+      install_requires=['MySQL_python'],
+      provides=['pysqlpool','PySQLPool'],
+      packages=['PySQLPool'],
+      package_dir={'PySQLPool': 'PySQLPool/PySQLPool'}
+     )
